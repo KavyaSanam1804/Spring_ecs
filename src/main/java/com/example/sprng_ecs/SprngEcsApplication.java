329ffa656bedac2948105575e8061710e87411ad
KavyaSanam1804 <kavya180204@gmail.com>
2026-01-30 11:12:39 +0530

    initial commit

---
+++ src/main/java/com/example/sprng_ecs/SprngEcsApplication.java
@@ -0,0 +1,13 @@
+package com.example.sprng_ecs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprngEcsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprngEcsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sprng_ecs/SprngEcsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sprng_ecs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprngEcsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
